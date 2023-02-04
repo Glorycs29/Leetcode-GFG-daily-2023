aa60f4ceffabe485143bd4ca9441c1bea4b2feec
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-04 16:33:17 +0530

    Create Subsets(Leetcode)(Bit_manipulation).cpp
    Bit Manipulation
    
    To give all the possible subsets, we just need to exhaust all the possible combinations of the numbers. And each number has only two possibilities: either in or not in a subset. And this can be represented using a bit.
    
    Using [1, 2, 3] as an example, 1 appears once in every two consecutive subsets, 2 appears twice in every four consecutive subsets, and 3 appears four times in every eight subsets (initially all subsets are empty):
    
    [], [ ], [ ], [    ], [ ], [    ], [    ], [       ]
    [], [1], [ ], [1   ], [ ], [1   ], [    ], [1      ]
    [], [1], [2], [1, 2], [ ], [1   ], [2   ], [1, 2   ]
    [], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]

---
+++ Subsets(Leetcode)(Bit_manipulation).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+      // 1<<n means  1*2^n
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
+    }
+};
