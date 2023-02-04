f818805c0abd42b232d8ed1cdd91ce44f435db3b
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-04 23:14:15 +0530

    Create Permutation in string(Leetcode).cpp
    Let us first redefine the problem.
    
    So permutation of a string basically means just the reordering of the letters of the string. That means all the permutations of a string are just the anagrams of the string, with all the letters included, just in another way.
    And we have to find if such a substring exists in s2 which is a permutation of s1, which means we have to find a substring in s2 such that the frequency of the characters in that substring is same as the frequency of the characters in s1.
    So we will be using SLIDING WINDOW APPROACH to find such a window in s2 in which the freq of characters is same as s1.
    
    Algorithm:
    1. The first step is to find the freq of all characters of s1.
    
    2. Then we will be starting with a window of size 1 initially in s2. That means start=0, end=0.
    
    3. Along with this, we will also be maintaining the frequency of the characters in the current window.
    
    4. We will be continuing the below steps untill we reach a situation where the end of the window reaches the end of s2. That means we will be doing the steps while end<length of s2.
    		a) Increase the frequency of the character which is just now newly included inside the window. That means increase the frequency of s2[end].
    		b) Now check if the frequency of the characters in the current window, is same as the frequency of characters int s1. But this is only possible if the length of current substring(window) is same as the length of s1. If this is true, then we can directly return true from here.
    		c) If the frequency doesnt match, we have to change the window:-
    					i) If the length of window in less than the length of s1, then we should increase the length of the the window by increasing the end. So end+=1.
    					ii) If not, that means length of window is greater than or equal to the length of s1, so we will need to move to a new window. For that we will have to move start to the next character but before that we will have to decrease the frequency of start character from the curr window frequency. That means
    							-Decrese the frequency of s2[start].
    							-Move start to the next element.
    							-Move end to the next element.
    							
    5. If the algorithm did not return true for any window, then we will reach here(out of the loop). This will mean that we did not find any such substring in s2. So return false.

---
+++ Permutation in string(Leetcode).cpp
@@ -0,0 +1,60 @@
+class Solution {
+    bool areVectorsEqual(vector<int> a, vector<int> b){
+        for(int i=0; i<26; i++){
+            if(a[i]!=b[i]) return false;
+        }
+        return true;
+    }
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size()<s1.size()) return false;
+        vector<int> freqS1(26, 0);
+        for(char c: s1) freqS1[c-'a']++;
+        
+        vector<int> freqS2(26, 0);
+        int i=0, j=0;
+        
+        while(j<s2.size()){
+            freqS2[s2[j]-'a']++;
+            
+            if(j-i+1==s1.size()){
+                if(areVectorsEqual(freqS1, freqS2)) return true;
+            }
+            
+            if(j-i+1<s1.size()) j++;
+            else{
+                freqS2[s2[i]-'a']--;
+                i++;
+                j++;
+            }
+        }
+        return false;
+    }
+};
+
+
+// approach 2 
+
+// class Solution {
+// public:
+//     bool checkInclusion(string s1, string s2) {
+//         int n1=s1.size();
+//         int n2=s2.size();
+//         if(n1>n2)return false;
+
+//          sort(s1.begin(),s1.end());
+
+//          for(int i=0;i<n2-n1+1;i++){
+//         //     //take substring of size n1 from s2 string staring from i to n1 length
+//              string str=s2.substr(i,n1);
+//              //now sort the substring
+//              sort(str.begin(),str.end());
+
+//              if(str==s1){
+//                  return true;
+//              }
+//          }
+//          return false;
+//     }
+
+// };
