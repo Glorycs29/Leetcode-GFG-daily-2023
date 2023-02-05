4d4ffbd7e3c33d811b447761e702f7fbffd6e482
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-05 21:59:43 +0530

    Create Delete_middle_of stack(GFG).cpp

---
+++ Delete_middle_of stack(GFG).cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        int mid = ceil((sizeOfStack+1)/2);
+        if(s.size()==mid){
+            s.pop();
+            return;
+        }
+        int x = s.top();
+        s.pop();
+        deleteMid(s,sizeOfStack);
+        s.push(x);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
