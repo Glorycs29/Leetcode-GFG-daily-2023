d092ba1322ba54b8615597cd78d6da77baa685cc
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-05 21:18:16 +0530

    Create subsets(Recursive+iterative).cpp

---
+++ subsets(Recursive+iterative).cpp
@@ -0,0 +1,43 @@
+//////////////Recursive (Backtracking)
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
+    }
+};
+
+//////////////Iterative
+
+Using [1, 2, 3] as an example, the iterative process is like:
+
+Initially, one empty subset [[]]
+Adding 1 to []: [[], [1]];
+Adding 2 to [] and [1]: [[], [1], [2], [1, 2]];
+Adding 3 to [], [1], [2] and [1, 2]: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs = {{}};
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; i++) {
+                subs.push_back(subs[i]); 
+                subs.back().push_back(num);
+            }
+        }
+        return subs;
+    }
+}; 
