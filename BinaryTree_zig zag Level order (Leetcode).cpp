dd045d70eefc555d581ae8fe4583a60fc19bf663
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-19 15:14:09 +0530

    Create BinaryTree_zig zag Level order (Leetcode).cpp

---
+++ BinaryTree_zig zag Level order (Leetcode).cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+    	queue<TreeNode*> q;
+    	
+    	if(root == NULL) return res;
+    	
+    	q.push(root);
+    	int level = 0;
+    	while(!q.empty()){
+            vector<int> ans;
+    	    int size = q.size();
+    	    stack<TreeNode*> st;
+    	    
+    	    for(int i=0;i<size;i++){
+    	        TreeNode* cur = q.front();
+    	        q.pop();
+    	        
+    	        ans.push_back(cur->val);
+    	        
+    	        if(level%2 ==0){
+    	            if(cur->left){
+    	                st.push(cur->left);
+    	            }
+    	            if(cur->right){
+    	                st.push(cur->right);
+    	            }
+    	        }else{
+    	            if(cur->right){
+    	                st.push(cur->right);
+    	            }
+    	            if(cur->left){
+    	                st.push(cur->left);
+    	            }
+    	        }
+    	    }
+            res.push_back(ans);
+    	    while(!st.empty()){
+    	        q.push(st.top());
+    	        st.pop();
+    	    }
+    	    level++;
+    	}
+        return res;
+    
+    }
+};
