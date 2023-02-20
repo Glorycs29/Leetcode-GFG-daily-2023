e9cc9bdd8ac5e78579f2e4a4ff0abaf617fb13a4
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-20 19:11:03 +0530

    Create Search insert position(Leetcode).cpp
    Intuition
    As we know that lower bound of a shorted array gives index of either
    of target number (if present in array) or element larger than the target.
    This is what question is all about.
    The lower_bound and upper_bound function works on binary search so, time complexity is O(logn).
    Approach
    Complexity
    Time complexity: O(logn)
    Space complexity: O(1)

---
+++ Search insert position(Leetcode).cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int ans=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        return ans;
+        
+    }
+};
