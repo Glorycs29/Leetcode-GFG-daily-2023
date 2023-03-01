6da2a69dd9ccbddc327de027dabd9490cf9de15c
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-03-01 19:18:02 +0530

    Create Sort_an_array(Leetcode).cpp

---
+++ Sort_an_array(Leetcode).cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int low=0, high=nums.size()-1;
+        mergeSort(nums,low,high);
+        return nums;
+    }
+
+    void outPlaceMerge(vector<int> &nums, int low, int mid, int high) {
+	if (low >= high) return;
+	int l = low, r = mid + 1, k = 0, size = high - low + 1;
+	vector<int> sorted(size, 0);
+	while (l <= mid and r <= high)
+		sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
+	while (l <= mid) 
+		sorted[k++] = nums[l++];
+	while (r <= high) 
+		sorted[k++] = nums[r++];
+	for (k = 0; k < size; k++)
+		nums[k + low] = sorted[k];
+}
+
+void mergeSort(vector<int> &nums, int low, int high) {
+	if (low >= high) return;
+	int mid = (high - low) / 2 + low;
+	mergeSort(nums, low, mid);
+	mergeSort(nums, mid + 1, high);
+	outPlaceMerge(nums, low, mid, high);
+}
+};
