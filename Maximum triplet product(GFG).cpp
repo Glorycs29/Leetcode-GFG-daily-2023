81db2cdd223dbf0a2a69ce82cb5b73e872ed87f4
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-03-11 19:25:30 +0530

    Create Maximum triplet product(GFG).cpp

---
+++ Maximum triplet product(GFG).cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    long long maxTripletProduct(long long arr[], int n)
+    {
+    	long long mn1= INT_MAX, mn2= INT_MAX, mx1= INT_MIN, mx2= INT_MIN, mx3= INT_MIN;
+    	
+    	for(int p=0; p<n; p++){
+    	    // calculating first min and second minimum
+    	    if(arr[p]<mn1){
+    	        mn2=mn1;
+    	        mn1=arr[p];
+    	    }else if(arr[p]<mn2){
+    	        mn2=arr[p];
+    	    }
+    	    
+    	    // similarly finding max1 , max2, max3 elements
+    	    if(arr[p]>mx1){
+    	        mx3=mx2;
+    	        mx2=mx1;
+    	        mx1=arr[p];
+    	    }else if(arr[p]>mx2){
+    	        mx3=mx2;
+    	        mx2=arr[p];
+    	    }else if(arr[p]>mx3){
+    	        mx3=arr[p];
+    	    }
+    	}
+    	return max( (mn1*mn2*mx1), (mx1*mx2*mx3));
+    }
+};
+
+/*class Solution {
+  public:
+    
+    //naive approach O(nlogn)
+    /*
+    long long maxTripletProduct(long long arr[], int n)
+    {
+    	sort(arr,arr+n);
+    	
+    	long long mn1=arr[0],mn2=arr[1],mx1=arr[n-1],mx2=arr[n-2],mx3=arr[n-3];
+    	
+    	return max((mn1*mn2*mx1),(mx1*mx2*mx3));
+    }
+    */
