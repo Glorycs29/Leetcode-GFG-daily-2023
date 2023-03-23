99c38ee21e45a450626aad34b7aa76aa41a0a42e
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-03-23 19:50:39 +0530

    Create BST_maximum_difference(GFG).cpp
    The given formula is:
    
    diff = [sum of node.data from root to target ]  - [ sum of node.data from target to leaf ]
    
    one thing you can notice is, first part(root to target sum) in the formula is constant for a given target in the BST. The only variable thing is second part(target to leaf sum) . This is because the there are multiple leaves possible from target.
    
    So, in order to maximize the difference, we can minimize the second part in the formula😀.
    
    I've highlighted the above observation because our whole solution is going  to be dependent on it.
    
     
    
    Approach:
      Now, I'm going to divide the problem into two parts💭💭.
    1. Calculating the root to target sum.
    
    2. Minimize and Calculate the target to leaf sum.
    
    we can use DFS  to achieve do both the tasks.
    
     
    
    Below is the Self Explanatory Pseudo code for 1st part:
    
    /*if target exist then it gives root to target sum. 
    Else it returns a value indicating that target doesn't exist.*/
    
    /*The code is very similar to search a node in BST, 
    just the difference is maintaining sum from root to Leaf.*/
    
    function rootToTarget(root,target)
    {
        if(root is null) return (not found);
       if(root == target) return rootValue;
       
       if(target>root.data)   //move to right
                return root.data+rootToTarget( right of root );
       else  //move to left
               return root.data+rootToTarget( left of root ); 
    }
     
    
    Below is the Self Explanatory Pseudo code for 2nd part:
    
    function targetToLeaf(root)
    {
       if(root is null) return maxValue;
       if(root is leaf) return rootValue;
    
        int left=targetToLeaf(left of root);
        int right=targetToLeaf(right of root);
    
        return rootValue + min(left,right);    // I'll return minimum of left and right.
    }
     
    
    I'hope you have got the idea to solve the problem till now !!
    
     
    
    Time Complexity: O(N) 
    Space Complexity: O(H) (stack occupied by recursion Stack).

---
+++ BST_maximum_difference(GFG).cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int find(Node* root, int target, Node** targetNode) {
+        if (root == NULL) return INT_MIN;
+        if (root->data == target) {
+            *targetNode = root;
+            return target;
+        }
+        
+        if (target > root->data) {
+            return root->data + find(root->right, target, targetNode);
+        } else {
+            return root->data + find(root->left, target, targetNode);
+        }
+    }
+    
+    int solve(Node* root) {
+        if (root == NULL) return INT_MAX / 2;
+        if (root->left == NULL && root->right == NULL) return root->data;
+        
+        int left = root->data + solve(root->left);
+        int right = root->data + solve(root->right);
+        
+        return min(left, right);
+    }
+    
+    int maxDifferenceBST(Node* root, int target) {
+        Node* targetNode = NULL;
+        int rootToTarget = find(root, target, &targetNode);
+        
+        if (targetNode == NULL) return -1;
+        
+        int targetToLeaf = solve(targetNode);
+        
+        return rootToTarget - targetToLeaf;
+    }
+};
